2629d0067f3c52aa3d6cbabfa5fabf8c07931ab5
LinZC2097 <linzhichao_2813@163.com>
2020-02-13 16:51:45 -0500

    Decision tree without gategroical feature

--- Project/Decision_Tree_Pokemon.py
+++ Project/Decision_Tree_Pokemon.py
@@ -6,6 +6,8 @@ from pyspark.ml.feature import StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
 from pyspark.mllib.util import MLUtils
+from pyspark.mllib.regression import LabeledPoint
+import pandas as pd
 
 
 def main():
@@ -14,29 +16,32 @@ def main():
 
     sqlContext = SQLContext(sc)
 
+    spark = SparkSession.builder.appName('ml-bank').getOrCreate()
+    data = spark.read.csv('/Users/marsscho/Desktop/6364 Machine Learning/assignment/hw2/Pokemon.csv', header=True, inferSchema=True)
+    data.printSchema()
+
     data = sqlContext.read.format('com.databricks.spark.csv')\
         .options(header='true', inferschema='true')\
         .load('/Users/marsscho/Desktop/6364 Machine Learning/assignment/hw2/Pokemon.csv')
     data.show()
 
-    indexers = [StringIndexer(inputCol=column, outputCol=column + "_index").setHandleInvalid("keep").fit(data)
-                for column in ['Type 1', 'Type 2']]
+    # indexers = [StringIndexer(inputCol=column, outputCol=column + "_index").setHandleInvalid("keep").fit(data)
+    #             for column in ['Type1', 'Type2']]
+    #
+    #
+    # pipeline = Pipeline(stages=indexers)
+    # df_r = pipeline.fit(data).transform(data)
+    # print(type(df_r))
+    # # df_r.drop(['Type1', 'Type2'])
+    # df_r.drop(df_r.Type1).collect()
+    # df_r.show()
+    # print(df_r.Type1)
 
-    pipeline = Pipeline(stages=indexers)
-    df_r = pipeline.fit(data).transform(data)
-    print(type(df_r))
-    df_r.show()
-
-    rdd = df_r.rdd.map(tuple)
-    print(type(rdd))
+    rdd = data.rdd.map(lambda row: LabeledPoint(row[-1], row[:-1]))
 
     (trainingData, testData) = rdd.randomSplit([0.7, 0.3])
-    print(type(trainingData))
-    print(trainingData)
 
-    # pipeline = Pipeline(stages=[df_r, dt])
-    model = DecisionTree.trainClassifier(trainingData, numClasses=2,
-                                         categoricalFeaturesInfo={'Type 1_index', 'Type 2_index'},
+    model = DecisionTree.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={0, 1},
                                          impurity='gini', maxDepth=5, maxBins=32)
 
     # Evaluate model on test instances and compute test error
@@ -49,8 +54,8 @@ def main():
     print(model.toDebugString())
 
     # Save and load model
-    model.save(sc, "target/tmp/myDecisionTreeClassificationModel")
-    sameModel = DecisionTreeModel.load(sc, "target/tmp/myDecisionTreeClassificationModel")
+    # model.save(sc, "target/tmp/myDecisionTreeClassificationModel")
+    # sameModel = DecisionTreeModel.load(sc, "target/tmp/myDecisionTreeClassificationModel")
 
 
 
